75603d0f21fb98f15e1b7c73df9f17b641bfd505
Carolanne Yvon <elolithia@gmail.com>
2023-03-24 14:20:53 +0100

    Fin tp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,14 @@
+import { Square } from './square/square';
 import './app.scss';
 
 export class App {
   static run() {
-    console.log("App is running...");
+    const inputColor = document.querySelector('[type=color]') as HTMLInputElement;
+    const inputSize = document.querySelector('[type=range]') as HTMLInputElement;
+    const btSubmit = document.querySelector('button');
+
+    btSubmit.addEventListener('click', () => {
+      new Square(+inputSize.value, inputColor.value).render();
+    });
   }
 }
--- src/app/square/square.ts
+++ src/app/square/square.ts
@@ -1,4 +1,26 @@
 import './square.scss';
 
 export class Square {
+  private static _id: number = 0;
+  private squareId: number;
+
+  constructor(
+    private _size: number,
+    private _color: string = 'gray',
+  ) {
+    this.squareId = ++Square._id;
+  }
+
+  public render() {
+    const div = document.createElement('div');
+    div.id = `square-${this.squareId}`;
+    div.setAttribute('style', `
+      width: ${this._size}px;
+      height: ${this._size}px;
+      background: ${this._color};
+    `);
+    document
+      .querySelector('.squares')
+      .appendChild(div);
+  }
 }
